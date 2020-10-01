2d689fff636adaf38a02da709581b9912f3d843f
Gabriel Guimarães <67638212+galink2077@users.noreply.github.com>
2020-10-01 07:24:51 -0300

    Update Bondinho.c

--- Bondinho/Bondinho.c
+++ Bondinho/Bondinho.c
@@ -2,7 +2,7 @@
 
 int main(){
 	int A,M;//váriaveis dos Alunos e Monitores
-	scanf("%d%d",&A,&M);//Pega o valor e adiciona nas varaáveis, o scanf desse jeito permite escanear 2 variaveis
+	scanf("%d%d",&A,&M);//Pega o valor e adiciona nas varáveis, o scanf desse jeito permite escanear 2 ou mais variaveis ao mesmo tempo poupando código
 	if(A+M>50)//teste lógico
         printf("N");//if de uma linha não precisa de estar entre chaves
     else
